3c498a18041243ad463b7ae64154fb6df0556296
hong-kyungs <hkyungs11@naver.com>
2023-04-01 00:06:18 +0900

    로그인 폼 만들기

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
 
+import UserProfile from './UserProfile';
+import LoginForm from './LoginForm';
+
 const AppLayout = ({ children }) => {
+	//서버없이 로그인 유무를 확인할 수 없으므로, dummy data를 이용한다.
+	const [isLoggedIn, setIsloggedIn] = useState(false);
 	return (
 		<div>
 			<Menu mode='horizontal'>
@@ -28,7 +34,7 @@ const AppLayout = ({ children }) => {
 			</Menu>
 			<Row gutter={8}>
 				<Col xs={24} md={6}>
-					왼쪽 메뉴
+					{isLoggedIn ? <UserProfile /> : <LoginForm />}
 				</Col>
 				<Col xs={24} md={12}>
 					{children}
@@ -40,7 +46,7 @@ const AppLayout = ({ children }) => {
 					<a
 						href='https://hong-kyungs.github.io/'
 						target='_blank'
-						rel='noreferer noopener'>
+						rel='noreferrer noopener'>
 						My Blog
 					</a>
 				</Col>
