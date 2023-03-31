291ed842ab34df5ae101e6c61b31738718884231
hong-kyungs <hkyungs11@naver.com>
2023-04-01 00:49:55 +0900

    리렌더링 이해하기, styled-components 적용

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
 
 import UserProfile from './UserProfile';
 import LoginForm from './LoginForm';
+
+const SearchInput = styled(Input.Search)`
+	vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
 	//서버없이 로그인 유무를 확인할 수 없으므로, dummy data를 이용한다.
@@ -24,7 +29,7 @@ const AppLayout = ({ children }) => {
 					</Link>
 				</Menu.Item>
 				<Menu.Item>
-					<Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+					<SearchInput enterButton />
 				</Menu.Item>
 				<Menu.Item>
 					<Link href='/signup'>
