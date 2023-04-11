f5c7e953a725ef41ef717d5bdb288e3692ef3e83
hong-kyungs <hkyungs11@naver.com>
2023-04-11 17:22:12 +0900

    컴포넌트 폴더 - 스타일 분리

--- prepare/front/components/ImagesZoom/index.js
+++ prepare/front/components/ImagesZoom/index.js
@@ -1,79 +1,16 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slick from 'react-slick';
-import styled, { createGlobalStyle } from 'styled-components';
-import { CloseOutlined } from '@ant-design/icons';
 
-const Overlay = styled.div`
-	position: fixed;
-	z-index: 5000;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-`;
-
-const Header = styled.header`
-	height: 44px;
-	background: white;
-	position: relative;
-	padding: 0;
-	text-align: center;
-
-	& h1 {
-		margin: 0;
-		font-size: 17px;
-		color: #333;
-		line-height: 44px;
-	}
-`;
-
-//antd component를 스타일링 하기 위해서 CloseBtn로 따로 빼줌
-const CloseBtn = styled(CloseOutlined)`
-	position: absolute;
-	right: 0;
-	top: 0;
-	padding: 15px;
-	line-height: 14px;
-	cursor: pointer;
-`;
-
-const SlickWrapper = styled.div`
-	height: calc(100% - 44px);
-	background: #090909;
-`;
-
-const ImgWrapper = styled.div`
-	padding: 32px;
-	text-align: center;
-
-	& img {
-		margin: 0 auto;
-		max-height: 750px;
-	}
-`;
-
-const Indicator = styled.div`
-	text-align: center;
-
-	& > div {
-		width: 75px;
-		height: 30px;
-		line-height: 30px;
-		border-radius: 15px;
-		background: #313131;
-		display: inline-block;
-		text-align: center;
-		color: white;
-		font-size: 15px;
-	}
-`;
-
-const Global = createGlobalStyle`
-  .slick-slide{
-    display: inline-block;
-  }
-`;
+import {
+	Overlay,
+	Header,
+	CloseBtn,
+	SlickWrapper,
+	ImgWrapper,
+	Indicator,
+	Global,
+} from './styles';
 
 const ImagesZoom = ({ images, onClose }) => {
 	const [currentSlide, setCurrentSlide] = useState(0);
@@ -103,7 +40,7 @@ const ImagesZoom = ({ images, onClose }) => {
 					</Slick>
 					<Indicator>
 						<div>
-							{currentSlide + 1} /{images.length}
+							{currentSlide + 1} / {images.length}
 						</div>
 					</Indicator>
 				</div>
