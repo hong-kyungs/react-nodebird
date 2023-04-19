6a2435b6d51350501233b0c7c88e777e49bc1f46
hong-kyungs <hkyungs11@naver.com>
2023-04-19 18:12:34 +0900

    faker로 실감나는 더미데이터 만들기

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
@@ -8,7 +9,17 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
 	const me = useSelector((state) => state.user);
+	//로그아웃 할 경우 - 메인페이지로 보내기
+	useEffect(() => {
+		if (!(me && me.id)) {
+			Router.push('/');
+		}
+	}, [me && me.id]);
 
+	//로그인 하지 않으면, 프로필에 아무일도 일어나지 않게하기
+	if (!me) {
+		return null;
+	}
 	return (
 		<>
 			<Head>
