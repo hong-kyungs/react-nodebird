c762b7989e3ad166767985c0c6d4c21c8bf5f5e2
hong-kyungs <hkyungs11@naver.com>
2023-04-28 01:11:21 +0900

    시퀄라이즈 sync + nodemon

--- prepare/back/models/post.js
+++ prepare/back/models/post.js
@@ -17,11 +17,11 @@ module.exports = (sequelize, DataTypes) => {
 		db.Post.belongsTo(db.User);
 		db.Post.hasMany(db.Comment);
 		db.Post.hasMany(db.Image);
-		db.Post.belongsToMany(db.Hashtag);
+		db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
 		//Likers는 게시글에 좋아요를 누른 사람들.
 		db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' });
 		//리트윗한 게시글
-		db.Post.belongsTo(db.Post, { as: Retweet });
+		db.Post.belongsTo(db.Post, { as: 'Retweet' });
 	};
 	return Post;
 };
