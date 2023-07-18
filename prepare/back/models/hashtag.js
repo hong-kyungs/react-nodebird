347d8a4eba36348e243f9ea8a46f4b9729adfbe4
hong-kyungs <hkyungs11@naver.com>
2023-07-18 18:51:37 +0900

    5-12.배포전 Q&A - model 최신문법으로 변경하기

--- prepare/back/models/hashtag.js
+++ prepare/back/models/hashtag.js
@@ -1,20 +1,26 @@
-module.exports = (sequelize, DataTypes) => {
-	const Hashtag = sequelize.define(
-		'Hashtag',
-		{
-			name: {
-				type: DataTypes.STRING(20),
-				allowNull: false,
-			},
-		},
-		{
-			charset: 'utf8mb4',
-			collate: 'utf8mb4_general_ci', // 한글+이모티콘 저장
-		}
-	);
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
 
-	Hashtag.associate = (db) => {
+module.exports = class Hashtag extends Model {
+	static init(sequelize) {
+		return super.init(
+			{
+				name: {
+					type: DataTypes.STRING(20),
+					allowNull: false,
+				},
+			},
+			{
+				modelName: 'Hashtag',
+				tableName: 'hashtags',
+				charset: 'utf8mb4',
+				collate: 'utf8mb4_general_ci', // 한글+이모티콘 저장
+				sequelize,
+			}
+		);
+	}
+
+	static associate(db) {
 		db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
-	};
-	return Hashtag;
+	}
 };
--- prepare/back/models/post.js
+++ prepare/back/models/post.js
@@ -1,19 +1,26 @@
-module.exports = (sequelize, DataTypes) => {
-	const Post = sequelize.define(
-		'Post',
-		{
-			content: {
-				type: DataTypes.TEXT, //TEXT를 넣으면 글자 무제한
-				allowNull: false,
-			},
-		},
-		{
-			charset: 'utf8mb4',
-			collate: 'utf8mb4_general_ci', // 한글+이모티콘 저장
-		}
-	);
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
 
-	Post.associate = (db) => {
+module.exports = class Post extends Model {
+	static init(sequelize) {
+		return super.init(
+			{
+				content: {
+					type: DataTypes.TEXT, //TEXT를 넣으면 글자 무제한
+					allowNull: false,
+				},
+			},
+			{
+				modelName: 'Post',
+				tableName: 'posts',
+				charset: 'utf8mb4',
+				collate: 'utf8mb4_general_ci', // 이모티콘 저장
+				sequelize,
+			}
+		);
+	}
+
+	static associate(db) {
 		db.Post.belongsTo(db.User); //post.addUser, post.getUser, post.setUser
 		db.Post.hasMany(db.Comment); //post.addComments, post.getComments
 		db.Post.hasMany(db.Image); //post.addImages, post.getImages
@@ -22,6 +29,5 @@ module.exports = (sequelize, DataTypes) => {
 		db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' }); //post.addLikers,  post.removeLikers
 		//리트윗한 게시글
 		db.Post.belongsTo(db.Post, { as: 'Retweet' }); //post.addRetweet
-	};
-	return Post;
+	}
 };
