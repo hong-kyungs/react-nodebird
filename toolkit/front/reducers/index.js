e5060a1c13a8ca2b4643e29e56388b9b8f869562
hong-kyungs <hkyungs11@naver.com>
2023-10-16 19:09:07 +0900

    toolkit - index 원래 상태로 되돌리기

--- toolkit/front/reducers/index.js
+++ toolkit/front/reducers/index.js
@@ -4,24 +4,9 @@ import { combineReducers } from 'redux';
 import userSlice from './userSlice';
 import postSlice from './postSlice';
 
-//(이전상태, 액션) => 다음상태
-//리듀서는 이전상태와 액션을 받아서 다음상태를 만들어내는 함수
-const rootReducer = (state, action) => {
-	switch (action.type) {
-		//server side rendering을 위해 hydrate를 넣어줘야함.
-		//HYDRATE의 등장은 SSR 위한 것으로, getInitialProps 와 getServerSideProps에서도 Redux store에 접근이 가능하도록 하기 위한 처리.
-		case HYDRATE:
-			console.log('HYDRATE', action);
-			return action.payload;
-		default: {
-			const combineReducer = combineReducers({
-				// 분리된 redecer를 combineReducers를 사용해 합쳐준다.
-				user: userSlice.reducer,
-				post: postSlice.reducer,
-			});
-			return combineReducer(state, action);
-		}
-	}
-};
+const rootReducer = combineReducers({
+	user: userSlice.reducer,
+	post: postSlice.reducer,
+});
 
 export default rootReducer;
